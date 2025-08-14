41e278f374196ce8585fa7f91cbb30a8d59cd09b
Ruslan Priadko <r.priadko@ukr.net>
2025-08-14 12:16:44 +0300

    Create worker-pattern and worker-views skeleton

--- manager/urls.py
+++ manager/urls.py
@@ -1,13 +1,28 @@
-from django.urls import path
+from django.urls import path, include
 from .views import (
     index,
+    WorkerListView,
+    WorkerDetailView,
+    WorkerCreateView,
+    WorkerUpdateView,
+    WorkerDeleteView,
 )
 
 
 app_name = "manager"
 
 
+worker_patterns = [
+    path('', WorkerListView.as_view(), name="workers-list"),
+    path('<int:pk>/', WorkerDetailView.as_view(), name="worker-detail"),
+    path('create/', WorkerCreateView.as_view(), name="worker-create"),
+    path('<int:pk>/update/', WorkerUpdateView.as_view(), name="worker-update"),
+    path('<int:pk>/delete/', WorkerDeleteView.as_view(), name="worker-delete"),
+]
+
+
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("workers/", include(worker_patterns)),
 ]
 
--- manager/views.py
+++ manager/views.py
@@ -1,6 +1,35 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
+from manager.models import (
+    Worker
+)
 
 def index(request):
     return HttpResponse("Some test text")
+
+
+class WorkerListView(generic.ListView):
+    model = Worker
+
+    class Meta:
+        ordering = ['username']
+
+
+class WorkerDetailView(generic.DetailView):
+    model = Worker
+
+
+class WorkerCreateView(generic.CreateView):
+    model = Worker
+    fields = '__all__'
+
+
+class WorkerUpdateView(generic.UpdateView):
+    model = Worker
+
+
+class WorkerDeleteView(generic.DeleteView):
+    model = Worker
+    success_url = reverse_lazy("manager:workers-list")
