c600a1fedccee8feda1eb8ced10e761942e2f5e0
Ruslan Priadko <r.priadko@ukr.net>
2025-08-19 11:54:30 +0300

    Add displaying of uncomleted-tasks-only feature to TaskDisplayView.

--- manager/views.py
+++ manager/views.py
@@ -1,16 +1,17 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.urls import reverse_lazy, reverse
+from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 
 from manager.forms import WorkerForm, TaskForm
 from manager.models import (
     Worker,
     Task,
-    Position,
-    TaskType,
 )
 
+
+@login_required
 def index(request):
     tasks_count = Task.objects.count()
     tasks_incomplete_count = Task.objects.filter(is_completed=False).count()
@@ -24,55 +25,60 @@ def index(request):
 
 
 
-
-class WorkerListView(generic.ListView):
+class WorkerListView(LoginRequiredMixin, generic.ListView):
     model = Worker
 
     class Meta:
         ordering = ['username']
 
 
-class WorkerDetailView(generic.DetailView):
+class WorkerDetailView(LoginRequiredMixin, generic.DetailView):
     model = Worker
 
 
-class WorkerCreateView(generic.CreateView):
-    model = Worker
-    form_class = WorkerForm
-    success_url = reverse_lazy("manager:workers-list")
-
-
-class WorkerUpdateView(generic.UpdateView):
+class WorkerCreateView(LoginRequiredMixin, generic.CreateView):
     model = Worker
     form_class = WorkerForm
     success_url = reverse_lazy("manager:workers-list")
 
 
-class WorkerDeleteView(generic.DeleteView):
+class WorkerUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Worker
+    form_class = WorkerForm
+    success_url = reverse_lazy("manager:workers-list")
+
+
+class WorkerDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Worker
     template_name = "manager/confirm_delete.html"
     success_url = reverse_lazy("manager:workers-list")
 
 
-class TaskListView(generic.ListView):
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.GET.get("uncompleted"):
+            queryset = queryset.filter(is_completed=False)
+        return queryset
+
+
+class TaskDetailView(LoginRequiredMixin, generic.DetailView):
     model = Task
 
 
-class TaskDetailView(generic.DetailView):
-    model = Task
-
-
-class TaskCreateView(generic.CreateView):
+class TaskCreateView(LoginRequiredMixin, generic.CreateView):
     model = Task
     form_class = TaskForm
     success_url = reverse_lazy("manager:task-list")
 
-class TaskUpdateView(generic.UpdateView):
+class TaskUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Task
     form_class = TaskForm
     success_url = reverse_lazy("manager:tasks-list")
 
-class TaskDeleteView(generic.DeleteView):
+class TaskDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Task
     template_name = "manager/confirm_delete.html"
     success_url = reverse_lazy("manager:task-list")
