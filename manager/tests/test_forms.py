a5517e3cc503e6c66c553d8c647ff059b88a22a4
Ruslan Priadko <r.priadko@ukr.net>
2025-08-22 13:15:45 +0300

    Impement tests

---
+++ manager/tests/test_forms.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.forms import DateTimeInput
+
+from manager.forms import TaskForm, WorkerForm
+
+
+class TestForms(TestCase):
+    def setUp(self):
+        self.task_form = TaskForm()
+        self.worker_form = WorkerForm()
+
+    def test_task_form_contain_fields(self):
+        expected_fields = [
+            "name",
+            "description",
+            "deadline",
+            "is_completed",
+            "priority",
+            "task_type",
+            "assignees"
+        ]
+        for field in expected_fields:
+            self.assertIn(field, self.task_form.fields)
+
+    def test_task_form_deadline_widget(self):
+        widget = self.task_form.fields['deadline'].widget
+        self.assertIsInstance(widget, DateTimeInput)
+        self.assertEqual(widget.input_type, "datetime-local")
+
+    def test_worker_form_contain_fields(self):
+        expected_fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "position"
+        ]
+        for field in expected_fields:
+            self.assertIn(field, self.worker_form.fields)
---
+++ manager/tests/test_views.py
@@ -0,0 +1,82 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.utils.timezone import now
+
+from manager.models import Position, Task, TaskType
+
+
+GROUP_URL_NAMES = [
+    "workers-list",
+    "worker-create",
+    "task-list",
+    "index",
+    "task-create",
+]
+
+DETAILED_URL_VIEWS = [
+    "worker-detail",
+    "worker-update",
+    "worker-delete",
+    "task-detail",
+    "task-update",
+    "task-delete",
+]
+
+
+APP_NAME = "manager"
+
+
+class PublicViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_views_unauthorized(self):
+        for url_name in DETAILED_URL_VIEWS:
+            url = reverse(f"{APP_NAME}:{url_name}", args=[1, ])
+            response = self.client.get(url)
+            self.assertNotEquals(response.status_code, 200)
+
+        for url_name in GROUP_URL_NAMES:
+            url = reverse(f"{APP_NAME}:{url_name}")
+            response = self.client.get(url)
+            self.assertNotEquals(response.status_code, 200)
+
+
+class PrivateViewTest(TestCase):
+    def setUp(self):
+        self.position = Position.objects.create(name="TestPosition")
+
+        self.user = get_user_model().objects.create_user(
+            password="pbkdf",
+            username="alice",
+            first_name="Alice",
+            last_name="Smith",
+            email="alice@example.com",
+            is_staff=False,
+            is_active=True,
+            date_joined="2025-08-13T12:00:00Z",
+            position=self.position,
+        )
+        self.user.save()
+
+        self.task_type = TaskType.objects.create(
+            name="TestTaskType",
+        )
+
+        self.task = Task.objects.create(
+            name="TestTask",
+            description="TestTask Description",
+            deadline=now(),
+            is_completed=False,
+            priority="UG",
+            task_type=self.task_type,
+        )
+        self.task.assignees.set([self.user])
+
+        self.client.force_login(self.user)
+
+    def test_uses_correct_template(self):
+        response = self.client.get(reverse(f"{APP_NAME}:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "manager/index.html")
