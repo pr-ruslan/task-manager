5f1e7a8745106fa7aaf353040876ca3044ed9de8
Ruslan Priadko <r.priadko@ukr.net>
2025-08-22 15:05:39 +0300

    Add some tests for search forms.

--- manager/tests/test_views.py
+++ manager/tests/test_views.py
@@ -80,3 +80,26 @@ class PrivateViewTest(TestCase):
         response = self.client.get(reverse(f"{APP_NAME}:index"))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "manager/index.html")
+
+    def test_worker_list_view(self):
+        res = self.client.get(reverse("manager:workers-list"))
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, "manager/worker_list.html")
+        self.assertEqual(list(res.context["worker_list"]), [self.user])
+
+    def test_worker_create_view(self):
+        res = self.client.post(reverse("manager:worker-create"), {
+            "username": "newworker",
+            "password": "password123",
+            "first_name": "New",
+            "last_name": "Worker",
+            "position": self.position.pk, # Add this line
+        })
+        self.assertRedirects(res, reverse("manager:workers-list"))
+        self.assertTrue(get_user_model().objects.filter(username="newworker").exists())
+
+    def test_worker_list_view_search(self):
+        res = self.client.get(reverse("manager:workers-list") + "?q=alic")
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(len(res.context["worker_list"]), 1)
+        self.assertEqual(res.context["worker_list"][0].username, "alice")
