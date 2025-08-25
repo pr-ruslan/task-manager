816efe11c94862f86fcb8f7cf4b9ff912a15f1a9
Ruslan Priadko <r.priadko@ukr.net>
2025-08-25 12:36:17 +0300

    Add .env, .env.samle, replaced SECRET_KEY in settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -1,5 +1,9 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-d=4dt^vxy#of-x^02**8-6tqrkr^kogw0(!t21k@@o64!2cdw8"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
