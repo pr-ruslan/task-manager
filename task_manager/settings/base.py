c2f0e5f7a0ca9ff8d80e74b0ced73e298d0ef225
Ruslan Priadko <r.priadko@ukr.net>
2025-08-26 16:50:45 +0300

    Manage static files

--- task_manager/settings/base.py
+++ task_manager/settings/base.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -96,9 +97,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = "staticfiles/"
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 # Default primary key field type
